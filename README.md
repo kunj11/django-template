8746dcae9876d933d898fcf79e3850421c061dc2
Arunaya Kunj <imkunj11@gmail.com>
2026-03-05 17:09:57 +0530

    django rest framework working template

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # django-template
+
+## Using Django Rest Framework
+1. Define models
+2. Define serializers
+3. Define views
+4. Define urls
+    - use router
+5. Include app urls in project urls
+    - path('', include(app.urls))
+6. Add the apps name in settings
+    - INSTALLED_APPS = [
+        'app',
+        'rest_framework'
+    ]
---
+++ app/views.py
@@ -0,0 +1,33 @@
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+
+from .models import *
+from .serializers import *
+
+
+class UserViewSet(viewsets.ViewSet):
+    permissions_classes = [permissions.AllowAny]
+
+    def list(self, request):
+        queryset = User.objects.all()
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = User.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def create(self, request):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def partial_update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
